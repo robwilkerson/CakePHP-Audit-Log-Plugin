d55fba23c4a8a2e1f651bdb46ec5cde9358a93d3
Rob Wilkerson <rob@robwilkerson.org>
2011-07-06 07:57:01 -0400

    Adding a few key unit tests. Hopefully more will follow.

---
+++ tests/fixtures/audit_delta_fixture.php
@@ -0,0 +1,21 @@
+<?php
+
+class AuditDeltaFixture extends CakeTestFixture {
+	public $name = 'AuditDelta';
+
+	public $fields = array(
+    'id'            => array( 'type' => 'string', 'length' => 36, 'null' => false ),
+    'audit_id'      => array( 'type' => 'string', 'length' => 36, 'null' => false ),
+    'property_name' => array( 'type' => 'string', 'length' => 255, 'null' => false ),
+    'old_value'     => array( 'type' => 'string', 'length' => 255 ),
+    'new_value'     => array( 'type' => 'string', 'length' => 255 ),
+	);
+
+/**
+ * records property
+ *
+ * @var array
+ * @access public
+ */
+	var $records = array();
+}
---
+++ tests/fixtures/audit_fixture.php
@@ -0,0 +1,24 @@
+<?php
+
+class AuditFixture extends CakeTestFixture {
+	public $name = 'Audit';
+
+	public $fields = array(
+    'id'          => array( 'type' => 'string', 'length' => 36, 'null' => false ),
+    'event'       => array( 'type' => 'string', 'length' => 255, 'null' => false ),
+    'model'       => array( 'type' => 'string', 'length' => 255, 'null' => false ),
+    'entity_id'   => array( 'type' => 'string', 'length' => 36, 'null' => false ),
+    'json_object' => array( 'type' => 'text', 'null' => false ),
+    'description' => array( 'type' => 'text' ),
+    'source_id'   => array( 'type' => 'string', 'length' => 255 ),
+    'created'     => array( 'type' => 'datetime' ),
+	);
+
+/**
+ * records property
+ *
+ * @var array
+ * @access public
+ */
+	var $records = array();
+}
