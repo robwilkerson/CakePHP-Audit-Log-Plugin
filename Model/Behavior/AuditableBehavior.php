ab5c168cb39459be2d50b6c4854f82e4d3869cd0
Wuilliam A. Lacruzm M <wuilliam321@gmail.com>
2013-10-03 10:38:08 -0430

    Add description field to AuditableBehavior model, in order to set custom value on it

--- Model/Behavior/AuditableBehavior.php
+++ Model/Behavior/AuditableBehavior.php
@@ -132,7 +132,8 @@ class AuditableBehavior extends ModelBehavior {
         'model'     => $Model->alias,
         'entity_id' => $Model->id,
         'json_object' => json_encode( $audit ),
-        'source_id' => isset( $source['id'] ) ? $source['id'] : null
+        'source_id' => isset( $source['id'] ) ? $source['id'] : null,
+        'description' => isset( $source['description'] ) ? $source['description'] : null,
       )
     );
 
